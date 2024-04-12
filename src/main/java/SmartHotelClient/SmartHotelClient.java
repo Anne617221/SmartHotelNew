941b37b5174cb2921a14c89878d3ba034b7404de
Anne617221 <annehong152@gmail.com>
2024-04-12 10:40:47 +0100

    change

--- src/main/java/SmartHotelClient/SmartHotelClient.java
+++ src/main/java/SmartHotelClient/SmartHotelClient.java
@@ -1,4 +1,71 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
 package SmartHotelClient;
 
 public class SmartHotelClient {
+    private final ManagedChannel channel;
+    private final SmartHotelClientblockingStub.blockingStub;
+    private final StreamingClientGrpc.StreamingServerServiceStub streamingStub;
+    public SmartHotelClient(String host,int port){
+        this.channel=ManagedChannelBuilder.forAddress(host,port)
+                .usePlaintext()
+                .build();
+        this.blockingStub=SmartHotelBlockingStub(channel);
+        this.streamingStub=streamingCilentGrpc.newStub(channel);
+    }
+
+    public void shutdown() {
+        try {
+            awaitTermination(5, TimeUnit.SECONDS);
+        } catch (InterruputedException e) {
+            System.err.println("Error while shutting down client:" + e.getMessage());
+        }
+    }
+    public void turnOnLight(){
+        LightRequest request=LightRequest.newBuilder().build();
+        LightResponse response=lightControlStub.turnOnLight(request);
+        System.out.println("Light turned on:"+response);
+    }
+
+    public void stremServerRequest(){
+        StreamObserver<StreamServerResponse>responseStreamObserver=new StreamObserver<StreamServerResponse>() {
+            @Override
+            public void onNext(StreamServerResponse streamServerResponse) {
+                System.out.println("Server message:"+response.getMessage());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.err.println("Error in server streaming:"+t.getMessage());
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Server streaming completed");
+            }
+        };
+        streamingStub.streamServerRequest(StreamServerRequest.newBuilder().setServerName("Server01").build(),responseObserver);
+    }
+
+    public static void main(String[] args){
+        SmartHotelClient client=new SmartHotelClient("localhost", 50051);
+        client.turnOnLight();
+        client.streamServerRequest();
+
+        Scanner scanner=new Scanner(System.in);
+        while(ture){
+            System.out.println("Press 'Q' to quit");
+            String input=scanner.nextLine();
+            if(input.equalsIgnoreCase("Q")){
+                client.shutdown();
+                break;
+            }
+        }
+    }
+}
 }
--- src/main/java/SmartHotelServer/LightControlServer.java
+++ src/main/java/SmartHotelServer/LightControlServer.java
@@ -1,4 +1,66 @@
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
 package SmartHotelServer;
 
 public class LightControlServer {
+    private static final int PORT=8080;
+    private Server server;
+
+    private static void main(String[]args) throws IOException, InterruptedException{
+        LightControlServer lightServer=new LightControlServer();
+        lightServer.start();
+
+        private void start()throws IOException,InterruptedException{
+            server = ServerBuilder.forPort(PORT)
+                    .addServer(new LightControlimpl())
+                    .build()
+                    .start();
+
+            System.out.println("Server started, listening on port"+PORT);
+        }
+        Runtime.getRuntime().addShutdownHook(new Thread(()->{
+            System.out.println("Shutting down gRPC server");
+            try{
+                server.shutdown().awaitTermination(60,TimeUnit.SECONDS);
+            }catch(InterruptedException e){
+                e.printStackTrace(System.err);
+            }
+        }));
+        server.awaitTermination();
+    }
+
+    public class LightControlImpl extends LightControlGrpc.LightControlImplBase {
+        @Override
+        public StreamObserver<LightRequest>turnOnLight(StreamObserver<LightResponse>responseObserver) {
+            return new StreamObserver<LightRequest>(){
+                @Override
+                public void onNext(LightRequest request){
+                    System.out.println("Received message from client:"+rquest.getMessage());
+                    for(int i=0;i<5;i++){
+                        LightResponse response = LightResponse.newBuilder()
+                                .setStatus("Light is turned on")
+                                .build();
+
+                        responseObserver.onNext(response);
+
+                    }
+                }
+                @Override
+                public void onError(Throwable t){
+                    System.err.println("Error from client:"+t.getMessage()):}
+
+                @Override
+                public void onCompleted(){
+                    System.out.println("Client stream completed");
+                    responseObserver.onCompleted();
+                }
+            };
+        }
+    }
 }
+
